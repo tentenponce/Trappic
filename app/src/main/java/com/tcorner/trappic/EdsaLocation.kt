b514fc5a542ef43cb5d340120472b009e482cd7d
tentenponce <poncetenten10@gmail.com>
2018-11-12 20:59:11 +0800

    WIP new location

--- app/src/main/java/com/tcorner/trappic/EdsaLocation.kt
+++ app/src/main/java/com/tcorner/trappic/EdsaLocation.kt
@@ -6,4 +6,7 @@ object EdsaLocation {
 
     const val SANTOLAN_LAT = 14.611059
     const val SANTOLAN_LNG = 121.055009
+
+    const val ORTIGAS_LAT = 14.587809
+    const val ORTIGAS_LNG = 121.056682
 }
